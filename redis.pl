2f069f2a0332d0b9da900a9df53aea4eaa3dcf7f
denis vasyliev <den.vasyliev@gmail.com>
2013-07-18 15:08:10 +0300

    18MSRN.ME REDIS POOL 180713-rev.1.0

--- redis.pl
+++ redis.pl
@@ -2,38 +2,13 @@
 #
 my $rev='MSRN.ME REDIS POOL 180713-rev.1.0';
 #
-use FCGI;
-use FCGI::ProcManager qw(pm_manage pm_pre_dispatch pm_post_dispatch);
-use WWW::Curl::Easy;
 use Redis;
-#use Net::Telnet();
 use DBI;
-use Data::Dumper;
-use IO::Socket;
-#use XML::Simple;
-#use XML::Bare::SAX::Parser;
-use XML::Bare;
-use JSON::XS;
-#Business::PayPal;
-use Digest::SHA qw(hmac_sha512_hex);
-use Digest::MD5 qw(md5_hex);
-use URI::Escape;
-use Text::Template;
-use Email::Valid;
-use Switch;
-use POSIX;
-use Time::Local;
-use Time::HiRes qw(gettimeofday);
-use Benchmark qw(:hireswallclock);
-use IO::File;
-use File::Copy;
-use Encode;
 use warnings;
 use strict;
 no warnings 'once';
 ####### CONF ####################################
 my $R0 = Redis->new;
-$R0->hset('CONF','rev',"$rev");
 my %CONF=$R0->HGETALL('CONF');
 #
 ####### FORK ####################################
@@ -56,13 +31,12 @@ print "$CONF{rev} READY at $$ debug level $CONF{debug}\n";
 		sub{ my ($value,$topic,$subscribtions)=@_;
 		undef my $SQL;
 			if ($topic eq 'CONF'){
-					$CONF{$value}=$R->hget('CONF',$value);
-					$R->ZADD('TID',$redis_start++,"[API-COMMAND-$topic]: $value -> $CONF{$value}");
-					$SQL=qq[UPDATE CONF a SET a.val="$CONF{$value}" WHERE a.key=\'$value\' limit 1];
+					$CONF{$value}=$R->HGET('CONF',$value);
 			}#if CONF
 				$SQL=qq[INSERT INTO SDR values ($value)] if $topic eq 'SDR';
 				$SQL=qq[INSERT INTO TID values ($value)] if $topic eq 'TID';
-			$dbh->do($SQL);
+			$dbh->do($SQL) if $topic ne 'CONF';
+			$R->ZADD('TID',$CONF{i}++,"[REDIS-SPOOL-$topic]: $value");
 				}#sub
 					);#subscribe
 $R0->wait_for_messages($CONF{redis_sync_timeout}) while $CONF{redis_pool}>0;
